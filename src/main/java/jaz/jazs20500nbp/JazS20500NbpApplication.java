de0f5a0b90d53a003e23de0cbb587b127cdaa09c
s20500-pj <s20500@pjwstk.edu.pl>
2021-07-03 16:15:50 +0200

    Initial commit

---
+++ src/main/java/jaz/jazs20500nbp/JazS20500NbpApplication.java
@@ -0,0 +1,13 @@
+package jaz.jazs20500nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS20500NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS20500NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jaz/jazs20500nbp/JazS20500NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package jaz.jazs20500nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS20500NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
